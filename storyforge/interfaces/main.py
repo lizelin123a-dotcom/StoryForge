6e4a3f50ae632bb6c7ff295fb2842542ef486511
lizelin123a-dotcom <lizelin123a@gmail.com>
2026-05-06 14:33:39 +0800

    fix: add material detail to notebook UI

--- storyforge/interfaces/main.py
+++ storyforge/interfaces/main.py
@@ -14,7 +14,7 @@ async def lifespan(app: FastAPI) -> AsyncIterator[None]:
     yield
 
 
-app = FastAPI(title="StoryForge", version="2.0.3", lifespan=lifespan)
+app = FastAPI(title="StoryForge", version="2.0.4", lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
